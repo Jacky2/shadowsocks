4e61ea247c63f9105b280b6baad0ed28f21488e8
zhangjt <383040590@qq.com>
2017-11-13 14:12:43 +0800

    add txt

--- apiconfig.py
+++ apiconfig.py
@@ -1,4 +1,5 @@
 ﻿# Config
+# 前端节点管理增加一个节点，增加的节点ID为多少就修改为多少
 NODE_ID = 1
 
 
@@ -12,15 +13,16 @@ MU_SUFFIX = 'zhaoj.in'
 MU_REGEX = '%5m%id.%suffix'
 
 SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
-API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
+API_INTERFACE = 'glzjinmod'  # glzjinmod, modwebapi  --默认为modwebapi 需要修改为glzjinmod
 
-WEBAPI_URL = 'https://zhaoj.in'
+WEBAPI_URL = 'https://zhaoj.in' # 自己网站的域名
 WEBAPI_TOKEN = 'glzjin'
 
 # mudb
 MUDB_FILE = 'mudb.json'
 
 # Mysql
+# 前端服务器所用的数据库配置
 MYSQL_HOST = '127.0.0.1'
 MYSQL_PORT = 3306
 MYSQL_USER = 'ss'
@@ -33,9 +35,11 @@ MYSQL_SSL_CERT = ''
 MYSQL_SSL_KEY = ''
 
 # API
+# 填写服务器域名
 API_HOST = '127.0.0.1'
 API_PORT = 80
 API_PATH = '/mu/v2/'
+# 填写前端配置对应的TOKEN
 API_TOKEN = 'abcdef'
 API_UPDATE_TIME = 60
 
